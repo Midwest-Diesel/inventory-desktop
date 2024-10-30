7e0f5f1c0ce4191e3653a25aeadc0c5c704047d1
Bennett Smrdel <bennett@midwestdiesel.com>
2024-10-30 16:02:53 -0500

    feat: Create email stuff page

---
+++ src/pages/email-stuff.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "@/components/Layout";
+
+
+export default function EmailStuff() {
+  return (
+    <Layout title="Email Stuff">
+      <div className="email-stuff-page">
+        
+      </div>
+    </Layout>
+  );
+}
