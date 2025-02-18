93716974bda356b03aab4eac5d00e46f9192f2c2
Bennett Smrdel <bennett@midwestdiesel.com>
2025-02-18 16:25:28 -0600

    chore: Change error message

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 interface Props {
   statusCode?: number
-  error?: Error
+  error?: any
 }
 
 
@@ -19,7 +19,7 @@ export default function Error({ statusCode, error }: Props) {
       <body style={{ alignContent: 'center' }}>
         <main style={{ padding: '1rem', margin: 'auto', borderRadius: '1rem', backgroundColor: '#424242', width: 'fit-content', height: 'fit-content' }}>
           <h2 style={{ color: '#f53535' }}>{ error?.name || `Error ${statusCode || 500}` }</h2>
-          <p>{ error?.message || 'An unexpected error has occurred.' }</p>
+          <p>{ error?.response.data || error?.message || 'An unexpected error has occurred.' }</p>
           <p>{ error?.stack }</p>
 
           <button style={{ width: 'fit-content' }} onClick={() => location.reload()}>Reload</button>
