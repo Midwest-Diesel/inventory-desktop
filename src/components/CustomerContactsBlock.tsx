942deefe92e8b1cdce39a2d11b8d59034bb1fc84
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-08 15:27:58 -0500

    refactor: Change contacts block to dropdown

--- src/components/CustomerContactsBlock.tsx
+++ src/components/CustomerContactsBlock.tsx
@@ -3,6 +3,7 @@ import Button from "./Library/Button";
 import { addCustomerContact, deleteContact, editContact, editCustomer, getCustomerById } from "@/scripts/controllers/customerController";
 import Input from "./Library/Input";
 import { confirm } from "@/scripts/config/tauri";
+import Select from "./Library/Select/Select";
 
 interface Props {
   customer: Customer
@@ -11,63 +12,46 @@ interface Props {
 
 
 export default function CustomerContactsBlock({ customer, setCustomer }: Props) {
-  const [contactPage, setContactPage] = useState(0);
-  const [contact, setContact] = useState<Contact>(customer.contacts[contactPage]);
+  const [contact, setContact] = useState<Contact>(null);
   const [isEditing, setIsEditing] = useState(false);
-  const [contactsData, setContactsData] = useState<Contact[]>(customer.contacts);
   
   useEffect(() => {
-    setContact(customer.contacts[contactPage]);
-  }, [contactPage, customer]);
+    setContact(customer.contacts.find((c) => c.name === customer.contact));
+  }, [customer]);
 
-  const handlePrevPage = () => {
-    if (contactPage > 0) {
-      setContactPage(contactPage - 1);
-    } else {
-      setContactPage(customer.contacts.length - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (contactPage < customer.contacts.length - 1) {
-      setContactPage(contactPage + 1);
-    } else {
-      setContactPage(0);
-    }
-  };
-
-  const handleSetContact = async () => {
-    setContact(customer.contacts[contactPage]);
-    const newCustomer = { ...customer, contact: customer.contacts[contactPage].name };
-    setCustomer(newCustomer);
-    await editCustomer(newCustomer);
+  const changeContact = async (e: any) => {
+    setContact(customer.contacts.find((c) => c.name === e.target.value));
+    await editCustomer({ ...customer, contact: e.target.value });
+    setCustomer({ ...customer, contact: e.target.value });
   };
 
   const handleNewContact = async () => {
     const name = prompt('Enter a contact name');
     if (!name) return;
     await addCustomerContact(customer.id, name);
-    location.reload();
+    const res = await getCustomerById(customer.id);
+    setCustomer(res);
   };
 
-  const handleEditContact = async () => {
-    for (const contact of contactsData) {
-      await editContact(contact);
-    }
+  const handleSaveContact = async () => {
+    await editContact(contact);
     setIsEditing(false);
     setCustomer(await getCustomerById(customer.id));
   };
 
   const handleCancelEdit = () => {
-    setContactsData(customer.contacts);
+    setContact(customer.contacts.find((c) => c.name === customer.contact));
     setIsEditing(false);
   };
 
   const handleDeleteContact = async () => {
     if (!await confirm('Are you sure you want to delete this?')) return;
-    await deleteContact(contactsData[contactPage].id);
-    if (contactsData[contactPage].name === customer.contact) await editCustomer({ ...customer, contact: null });
-    location.reload();
+    await deleteContact(contact.id);
+    setCustomer({...customer, contacts: customer.contacts.filter((c) => c.id !== contact.id)});
+    if (contact.name === customer.contact) {
+      await editCustomer({ ...customer, contact: null });
+      setCustomer({ ...customer, contact: null });
+    }
   };
 
 
@@ -79,40 +63,39 @@ export default function CustomerContactsBlock({ customer, setCustomer }: Props) 
         {!isEditing && <Button type="button" onClick={() => setIsEditing(true)} variant={['blue']}>Edit</Button>}
         {isEditing &&
           <>
-            <Button type="button" onClick={handleEditContact} variant={['save']}>Save</Button>
-            <Button type="button" onClick={handleCancelEdit} variant={['danger']}>Cancel</Button>
+            <Button type="button" onClick={handleSaveContact} variant={['save']}>Save</Button>
+            <Button type="button" onClick={handleCancelEdit}>Cancel</Button>
           </>
         }
+        <Button type="button" onClick={handleDeleteContact} variant={['danger']}>Delete</Button>
+        <Select
+          value={contact?.name}
+          onChange={changeContact}
+        >
+          <option value="">-- CONTACT NAME --</option>
+          {customer.contacts.map((contact: Contact) => {
+            return <option key={contact.id}>{ contact.name }</option>;
+          })}
+        </Select>
       </div>
 
       <div className="contacts-block">
-        <Button type="button" onClick={handleDeleteContact} variant={['danger']} style={{ marginBottom: '0.5rem' }}>Delete</Button>
         {isEditing ?
           <>
             <div className="contacts-block__row">
               <Input
                 variant={['small', 'thin', 'label-full-width', 'label-space-between', 'label-bold']}
                 label="Name"
-                value={contactsData[contactPage].name}
-                onChange={(e: any) => setContactsData(contactsData.map((c) => {
-                  if (c.id === contactsData[contactPage].id) {
-                    return { ...contactsData[contactPage], name: e.target.value };
-                  }
-                  return c;
-                }))}
+                value={contact.name}
+                onChange={(e: any) => setContact({ ...contact, name: e.target.value })}
               />
             </div>
             <div className="contacts-block__row">
               <Input
                 variant={['small', 'thin', 'label-full-width', 'label-space-between', 'label-bold']}
                 label="Position"
-                value={contactsData[contactPage].position}
-                onChange={(e: any) => setContactsData(contactsData.map((c) => {
-                  if (c.id === contactsData[contactPage].id) {
-                    return { ...contactsData[contactPage], position: e.target.value };
-                  }
-                  return c;
-                }))}
+                value={contact.position}
+                onChange={(e: any) => setContact({ ...contact, position: e.target.value })}
               />
             </div>
             <div className="contacts-block__row">
@@ -120,13 +103,8 @@ export default function CustomerContactsBlock({ customer, setCustomer }: Props) 
                 variant={['small', 'thin', 'label-full-width', 'label-space-between', 'label-bold']}
                 label="Email"
                 type="email"
-                value={contactsData[contactPage].email}
-                onChange={(e: any) => setContactsData(contactsData.map((c) => {
-                  if (c.id === contactsData[contactPage].id) {
-                    return { ...contactsData[contactPage], email: e.target.value };
-                  }
-                  return c;
-                }))}
+                value={contact.email}
+                onChange={(e: any) => setContact({ ...contact, email: e.target.value })}
               />
             </div>
             <div className="contacts-block__row">
@@ -134,59 +112,39 @@ export default function CustomerContactsBlock({ customer, setCustomer }: Props) 
                 variant={['small', 'thin', 'label-full-width', 'label-space-between', 'label-bold']}
                 label="Ext"
                 type="number"
-                value={contactsData[contactPage].ext}
-                onChange={(e: any) => setContactsData(contactsData.map((c) => {
-                  if (c.id === contactsData[contactPage].id) {
-                    return { ...contactsData[contactPage], ext: e.target.value };
-                  }
-                  return c;
-                }))}
+                value={contact.ext}
+                onChange={(e: any) => setContact({ ...contact, ext: e.target.value })}
               />
             </div>
             <div className="contacts-block__row">
               <Input
                 variant={['small', 'thin', 'label-full-width', 'label-space-between', 'label-bold']}
                 label="Notes"
-                value={contactsData[contactPage].notes}
-                onChange={(e: any) => setContactsData(contactsData.map((c) => {
-                  if (c.id === contactsData[contactPage].id) {
-                    return { ...contactsData[contactPage], notes: e.target.value };
-                  }
-                  return c;
-                }))}
+                value={contact.notes}
+                onChange={(e: any) => setContact({ ...contact, notes: e.target.value })}
               />
             </div>
           </>
           :
           <>
             <div className="contacts-block__row">
-              <p><strong>Name</strong></p>
-              <p>{ contact.name }</p>
-            </div>
-            <div className="contacts-block__row">
               <p><strong>Position</strong></p>
-              <p>{ contact.position }</p>
+              <p>{ contact?.position }</p>
             </div>
             <div className="contacts-block__row">
               <p><strong>Email</strong></p>
-              <p>{ contact.email }</p>
+              <p>{ contact?.email }</p>
             </div>
             <div className="contacts-block__row">
               <p><strong>Ext</strong></p>
-              <p>{ contact.ext }</p>
+              <p>{ contact?.ext }</p>
             </div>
             <div className="contacts-block__row">
               <p><strong>Notes</strong></p>
-              <p>{ contact.notes }</p>
+              <p>{ contact?.notes }</p>
             </div>
           </>
         }
-
-        <div className="contacts-block__buttons">
-          { customer.contacts.length > 1 && <Button type="button" onClick={() => handlePrevPage()}>Previous</Button> }
-          { customer.contacts.length > 0 && <Button type="button" onClick={handleSetContact}>Set as Contact</Button> }
-          { customer.contacts.length > 1 && <Button type="button" onClick={() => handleNextPage()}>Next</Button> }
-        </div>
       </div>
     </div>
   );
