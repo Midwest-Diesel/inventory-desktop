871f81a716f9a5502d6915f17f20bf5ec1c4db31
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-17 10:53:43 -0500

    chore: Increase part tag size

--- src/pages/part/[partNum].tsx
+++ src/pages/part/[partNum].tsx
@@ -431,7 +431,7 @@ export default function PartDetails() {
       }
 
       {partTagProps &&
-        <div ref={printRef} style={{ width: '4200px', marginTop: '10rem' }}>
+        <div ref={printRef} style={{ width: '350px', marginTop: '10rem' }}>
           <PartTag
             properties={partTagProps}
           />
