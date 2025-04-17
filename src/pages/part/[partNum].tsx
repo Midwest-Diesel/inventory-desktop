ce5b4fe2c994f50281bc488df093670721175f9e
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-17 15:52:46 -0500

    chore: Multiple part tag size greatly

--- src/pages/part/[partNum].tsx
+++ src/pages/part/[partNum].tsx
@@ -434,7 +434,7 @@ export default function PartDetails() {
       }
 
       {partTagProps &&
-        <div ref={printRef} style={{ width: '4000px', marginTop: '10rem' }}>
+        <div ref={printRef} style={{ width: '12000px', marginTop: '10rem' }}>
           <PartTag
             properties={partTagProps}
           />
