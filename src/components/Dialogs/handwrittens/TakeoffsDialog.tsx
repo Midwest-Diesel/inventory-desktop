fbba9494377c5ade61dcf6a7d638ea816ff3dc9f
Bennett Smrdel <bennett@midwestdiesel.com>
2025-06-11 11:07:51 -0500

    chore: Display part info on takeoff

--- src/components/Dialogs/handwrittens/TakeoffsDialog.tsx
+++ src/components/Dialogs/handwrittens/TakeoffsDialog.tsx
@@ -19,15 +19,16 @@ interface Props {
 export default function TakeoffsDialog({ open, setOpen, item, setHandwritten }: Props) {
   const params = useParams();
   const [qty, setQty] = useState<number | null>(item.qty);
-  const [cost, setCost] = useState<number | null>(item.cost);
-  const [changeCost, setChangeCost] = useState(false);
-  
+  const [part, setPart] = useState<Part | null>(null);
+
   useEffect(() => {
-    if (item.cost === 0.04) {
-      setChangeCost(true);
-      setCost(0.04);
+    const fetchData = async () => {
+      if (!item.partId) return;
+      const res = await getPartById(item.partId);
+      setPart(res);
     };
-  }, [item]);
+    fetchData();
+  })
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -38,21 +39,20 @@ export default function TakeoffsDialog({ open, setOpen, item, setHandwritten }: 
     if (!part) return;
     
     const surplus: Surplus | null = await getSurplusByCode(part.purchasedFrom ?? '');
-    if (surplus && surplus.price - Number(cost) <= 0) await zeroAllSurplusItems(surplus.code);
+    if (surplus && surplus.price - Number(item.cost) <= 0) await zeroAllSurplusItems(surplus.code);
     if (part.qty - Number(qty) <= 0) {
       const res = await getPartCostIn(part.stockNum ?? '');
       const partCostIn: PartCostIn | null = res.find((p: PartCostIn) => p.vendor === part.purchasedFrom) ?? null;
-      if (partCostIn) await editPartCostIn({ ...partCostIn, cost: Number(cost) });
+      if (partCostIn) await editPartCostIn({ ...partCostIn, cost: item.cost });
     } else {
       const newStockNum = part.stockNum?.split('').slice(0, 2).join('').toUpperCase() !== 'UP' ? part.stockNum : `${part.stockNum} (${formatDate(new Date())})`;
       await addPart({ ...part, qty: 0, stockNum: newStockNum }, true);
-      await addPartCostIn(newStockNum ?? '', Number(cost), null, part.purchasedFrom ?? '', 'PurchasePrice', '');
+      await addPartCostIn(newStockNum ?? '', Number(item.cost), null, part.purchasedFrom ?? '', 'PurchasePrice', '');
     }
 
     const res = await getHandwrittenById(Number(params.handwritten));
     setHandwritten(res);
     setOpen(false);
-    setChangeCost(false);
   };
 
 
@@ -65,31 +65,27 @@ export default function TakeoffsDialog({ open, setOpen, item, setHandwritten }: 
       y={-200}
       data-testid="takeoffs-dialog"
     >
-      <form onSubmit={handleSubmit}>
-        <Input
-          variant={['x-small', 'label-bold', 'label-stack']}
-          label="Qty"
-          value={qty ?? ''}
-          onChange={(e: any) => setQty(Math.max(Math.min(e.target.value, (item.qty ?? 0)), 1))}
-          type="number"
-          data-testid="qty"
-        />
-        {changeCost &&
+      {part &&
+        <form onSubmit={handleSubmit}>
+          <p><strong>Part Number:</strong> { part.partNum }</p>
+          <p><strong>Stock Number:</strong> { part.stockNum }</p>
+          <p><strong>Description:</strong> { part.desc }</p>
+          <p><strong>Qty on Hand:</strong> { part.qty }</p>
+          <br />
           <Input
-            variant={['small', 'label-bold']}
-            label="Change Cost"
-            value={cost ?? ''}
-            onChange={(e: any) => setCost(e.target.value)}
+            variant={['x-small', 'label-bold', 'label-stack', 'label-fit-content']}
+            label="Qty"
+            value={qty ?? ''}
+            onChange={(e: any) => setQty(Math.max(Math.min(e.target.value, (item.qty ?? 0)), 1))}
             type="number"
-            step="any"
-            data-testid="change-cost"
+            data-testid="qty"
           />
-        }
 
-        <div className="form__footer">
-          <Button type="submit" data-testid="submit-btn">Submit</Button>
-        </div>
-      </form>
+          <div className="form__footer">
+            <Button type="submit" data-testid="submit-btn">Submit</Button>
+          </div>
+        </form>
+      }
     </Dialog>
   );
 }
