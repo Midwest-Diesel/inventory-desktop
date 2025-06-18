f950402f1bf809dfb5865facf2bfd7ca021c8f54
Bennett Smrdel <bennett@midwestdiesel.com>
2025-06-18 11:20:02 -0500

    chore: Create loading fallback

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from '~react-pages';
+import Loading from './Library/Loading';
 
 
 export default function App() {
-  return useRoutes(routes);
+  return (
+    <Suspense fallback={<Loading />}>
+      { useRoutes(routes) }
+    </Suspense>
+  );
 }
