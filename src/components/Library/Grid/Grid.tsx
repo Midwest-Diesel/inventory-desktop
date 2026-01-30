295ef3e6be145cabc90afb9ee1f5cfcb5d54fb44
Bennett Smrdel <bennett@midwestdiesel.com>
2026-01-30 13:47:39 -0600

    chore: Remove set values for grid columns

--- src/components/Library/Grid/Grid.tsx
+++ src/components/Library/Grid/Grid.tsx
@@ -1,30 +1,19 @@
 import { generateClasses, parseClasses } from "@/scripts/tools/utils";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 interface Props {
   variant?: ('default')[]
   children: React.ReactNode
   className?: string
-  rows?: number
-  cols?: number
-  gap?: number
 }
 
 
-export default function Grid({ children, className = '', rows = 1, cols = 12, gap = 0, variant = [] }: Props) {
-  const ref = useRef(null) as any;
+export default function Grid({ children, className = '', variant = [] }: Props) {
   const classes = generateClasses(className, variant, 'grid');
-    
-  useEffect(() => {
-    const grid = ref.current as HTMLDivElement;
-    grid.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`);
-    grid.style.setProperty('grid-template-columns', `repeat(${cols}, 1fr)`);
-    grid.style.setProperty('grid-gap', `${gap}rem`);
-  }, []);
 
 
   return (
-    <div {...parseClasses(classes)} ref={ref}>
+    <div {...parseClasses(classes)}>
       { children }
     </div>
   );
