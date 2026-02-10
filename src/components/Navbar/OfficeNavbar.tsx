5325b90a2f347ee9b53feae99b6f064e5483968b
Bennett Smrdel <bennett@midwestdiesel.com>
2026-02-10 09:59:34 -0600

    chore: Make shop addons visible to all

--- src/components/Navbar/OfficeNavbar.tsx
+++ src/components/Navbar/OfficeNavbar.tsx
@@ -26,12 +26,8 @@ export default function OfficeNavbar({ user }: Props) {
       <NavDropdown label="Add Ons">
         <Link href="/add-ons/office/part">Office Parts</Link>
         <Link href="/add-ons/office/engine">Office Engines</Link>
-        {user.accessLevel >= 3 &&
-          <>
-            <Link href="/add-ons/shop/part">Shop Parts</Link>
-            <Link href="/add-ons/shop/engine">Shop Engines</Link>
-          </>
-        }
+        <Link href="/add-ons/shop/part">Shop Parts</Link>
+        <Link href="/add-ons/shop/engine">Shop Engines</Link>
       </NavDropdown>
       <Link href="/karmak" className="navbar__link">Accounting</Link>
       <Link href="/email-stuff" className="navbar__link">Email Stuff</Link>
