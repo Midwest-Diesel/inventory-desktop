ef1202cda80bcf624091b95f9421690c79aae42f
Bennett Smrdel <bennett@midwestdiesel.com>
2026-04-29 16:30:38 -0500

    test: Add warranty tests

--- src/tests/e2e/warranties.spec.ts
+++ src/tests/e2e/warranties.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { resetDb } from '../resetDatabase';
 import { goto } from '../utils';
 
